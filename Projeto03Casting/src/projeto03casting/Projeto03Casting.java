937910adfd130e8d3fc990073d3c6d535d2c7db4
Brusilv17 <54293168+Brusilv17@users.noreply.github.com>
2019-08-19 19:47:59 -0300

    Projeto Casting. Java

---
+++ Projeto03Casting/src/projeto03casting/Projeto03Casting.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projeto03casting;
+
+/**
+ *
+ * @author mathe
+ */
+public class Projeto03Casting {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double pi = 3.14;
+        int i = (int) pi;
+        
+        System.out.println(i);
+        System.out.println(pi);
+        
+        float j = 3.14f;
+        float k = 3;
+        float m = (float) pi;
+    }
+    
+}
